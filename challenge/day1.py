c3b61f6cf73536d15db26dea380799666569b65f
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-09-24 15:45:50 +0100

    challenge 1 solved

---
+++ challenge/day1.py
@@ -0,0 +1,11 @@
+
+answer = 'Y'
+while (answer == 'Y' or answer == "YES") :
+
+    print("Welcome to the Band Name Generator.")
+    city = input("What\'s name of the city you grew up in?\n")
+    pet = input("What\'s your pet\'s name?\n")
+    print(f"Your band name could be {city} {pet}")
+    answer = input("\n\n\n\n\nThis repl has exited, run again ?").upper()
+
+
