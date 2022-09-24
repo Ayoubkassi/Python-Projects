c0eb8ef91752e5f61b6aaf2907e88a956c044bfa
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-09-24 16:07:35 +0100

    add challenge 2

---
+++ challenge/challenge-002/day2.py
@@ -0,0 +1,9 @@
+import math 
+
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+num_people = float(input("How many people to split the bill? "))
+percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+amount = (total_bill * ((100+percentage)/100) ) / num_people
+amount = math.ceil(amount)
+print(f"Each person should pay: ${amount}")
