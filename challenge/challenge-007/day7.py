d61de067ddf037942adf5ba636c8d76db6ef87ae
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2022-09-25 13:39:55 +0100

    add decryption part

--- challenge/challenge-007/day7.py
+++ challenge/challenge-007/day7.py
@@ -2,7 +2,7 @@
 
 # [65,90,97,122]
 
-def caesar_cipher(word,num):
+def caesar_cipher_encryption(word,num):
 	answer = []
 	for letter in word:
 		older_ord = ord(letter)
@@ -25,7 +25,41 @@ def caesar_cipher(word,num):
 	return "".join(answer)
 
 
-answer = caesar_cipher("Aa",4) 
-print(answer)
+def caesar_cipher_decryption(word,num):
+	answer = []
+	for letter in word:
+		older_ord = ord(letter)
+		if older_ord>=65 and older_ord<=90 :
+			if (older_ord-num) < 65  :
+				sol = 91 - (65%(older_ord-num))
+				new_letter = chr(sol)
+				#print(new_letter)
+			else :
+				new_letter = chr(older_ord-num)
+		else :
+			if (older_ord-num) < 97 :
+				sol = 123 - (97%(older_ord-num))
+				new_letter = chr(sol)
+			else :
+				new_letter = chr(older_ord-num)
+
+		answer.append(new_letter)
+
+	return "".join(answer)
+
+
+
+print(caesar_cipher_decryption("qnuuxlxmnab",9))
+
+
+# print("Welcome To Caesar Cipher Encryption\n")
+# choice = input("Type 'encode' to encrypt, type 'decode' to decrypt : \n")
+# message = input("Type your message : \n")
+# shift = int(input("Type the shift number : \n"))
+# if choice == "encode" :
+#     print(f"Here's the encoded result: {caesar_cipher_encryption(message, shift)}")
+# else :
+#     print(f"Here's the decoded result: {caesar_cipher_decryption(message, shift)}")
+
 
 
